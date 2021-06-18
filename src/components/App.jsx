ed4446609ce7e131aa6b8d7ccd9bdd8bb15c3c26
James Alexander <60295314+James-Alexandr@users.noreply.github.com>
2021-06-18 17:33:05 -0500

    update update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from "react";
 
 function App() {
-  let [count, setCount] = useState(0);
+  let [count, setCount] = useState(1);
   const [one, two, three] = [1, 3, 88];
 
   console.log(three);
 
-  function increase() {
-    count++;
-    setCount(count);
-  }
+  // function increase() {
+  //   count++;
+  //   setCount(count);
+  // }
 
-  function deCrease() {
-    count--;
-    setCount(count);
-  }
+  // function deCrease() {
+  //   count--;
+  //   setCount(count);
+  // }
 
   return (
     <div className="container">
       <h1>{count}</h1>
-      <button onClick={increase}>+</button>
-      <button onClick={deCrease}>-</button>
+      {/* <button onClick={increase}>+</button> */}
+      {/* <button onClick={deCrease}>-</button> */}
+      <button onClick={() => setCount(++count)}>+</button>
+      <button onClick={() => setCount(--count)}>-</button>
     </div>
   );
 }
